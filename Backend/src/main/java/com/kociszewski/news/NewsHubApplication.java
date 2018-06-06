132722c9020f66e69686c10513a7742e8e9aa56e
Mateusz Kociszewski <mateusz1003@gmail.com>
2018-06-06 22:43:19 +0200

    Define API access and fix gitignore

--- Backend/src/main/java/com/kociszewski/news/NewsHubApplication.java
+++ Backend/src/main/java/com/kociszewski/news/NewsHubApplication.java
@@ -1,12 +1,24 @@
 package com.kociszewski.news;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NewsHubApplication {
 
+	@Value("${api.url}")
+	private String baseUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NewsHubApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.rootUri(baseUrl).build();
 	}
 }
