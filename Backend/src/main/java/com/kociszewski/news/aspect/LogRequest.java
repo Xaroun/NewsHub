1d0b1eee8de408693eacb9add0a1865a1b4f1e62
Mateusz Kociszewski <mateusz1003@gmail.com>
2018-06-07 23:12:45 +0200

    Add request logging using aspects

---
+++ Backend/src/main/java/com/kociszewski/news/aspect/LogRequest.java
@@ -0,0 +1,53 @@
+package com.kociszewski.news.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
+import java.time.Instant;
+
+/**
+ * Created by mateusz on 07.06.2018.
+ */
+@Component
+@Aspect
+public class LogRequest {
+
+    private static Logger logger = LogManager.getLogger();
+
+    @Pointcut("execution(public org.springframework.http.ResponseEntity com.kociszewski.news.controller.*.*(..))")
+    public void controllersWithResponseEntity() {
+        // pointcut declaration
+    }
+
+    @Around("controllersWithResponseEntity()")
+    public Object aroundRequest(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        Instant start = Instant.now();
+        ResponseEntity<?> responseEntity = (ResponseEntity<?>) proceedingJoinPoint.proceed();
+        Instant end = Instant.now();
+        Double invocationTimeInSeconds = Double.valueOf(Duration.between(start, end).toMillis()) / 1000;
+
+        String log = String.format("[%s] request on [%s] from [%s] took [%.2f]s with code [%s]",
+                request.getMethod(),
+                request.getRequestURI(),
+                request.getRemoteAddr(),
+                invocationTimeInSeconds,
+                responseEntity.getStatusCode());
+
+        logger.info(log);
+
+        return responseEntity;
+    }
+
+}
