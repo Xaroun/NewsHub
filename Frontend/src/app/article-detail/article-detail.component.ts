00756345d2069e270c63eb6a31a324cd5a0fb7bd
Mateusz Kociszewski <mateusz1003@gmail.com>
2018-06-12 19:17:08 +0200

    Separate article details into individual component

---
+++ Frontend/src/app/article-detail/article-detail.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { Article} from '../article';
+
+@Component({
+  selector: 'app-article-detail',
+  templateUrl: './article-detail.component.html',
+  styleUrls: ['./article-detail.component.css']
+})
+export class ArticleDetailComponent implements OnInit {
+
+  @Input() article: Article;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
